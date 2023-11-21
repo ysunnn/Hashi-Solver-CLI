248f05008c67c982cc53440031197dea5d8d6dbd
Lukas Mahr <Lukas.Mahr@campus.lmu.de>
2023-11-21 22:46:12 +0100

    Update main to tranform the graph to ast and print it

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from reader import read_puzzle_from_string, plot_puzzle
+from encoder import encode
+from reader import read_puzzle_from_string, plot_puzzle, to_graph
 
 puzzle_str = """7
 1.3..2.
@@ -10,3 +11,7 @@ puzzle_str = """7
 ..2.1.2"""
 islands, bridges = read_puzzle_from_string(puzzle_str)
 plot_puzzle(islands, bridges)
+graph = to_graph(islands, bridges)
+
+expression, mapping = encode(graph=graph)
+print(expression)
