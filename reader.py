52b8b1a21defb17c8663acbee2f441d3d7982d99
Lukas Mahr <Lukas.Mahr@campus.lmu.de>
2023-11-21 22:37:20 +0100

    Update Island class to have a name Add function to convert island and bridges to grpah Format

--- reader.py
+++ reader.py
@@ -5,26 +5,32 @@ import matplotlib.pyplot as plt
 
 
 class Island:
-    def __init__(self, x, y, number_of_bridges):
+    def __init__(self, x, y, number_of_bridges, name):
         self.x = x
         self.y = y
         self.number_of_bridges = number_of_bridges
+        self.name = name
 
     def __repr__(self):
         return f"Island(x: {self.x, self.y, self.number_of_bridges}"
+
+    def __str__(self):
+        return self.name
 
 
 # Convert puzzle text to array with Island
 def _find_islands(puzzle_lines):
     islands = []
+    counter = 0
     for r, col in enumerate(puzzle_lines):
         my_col = []
         for c, ele in enumerate(col.strip()):
-            if ele == '.':
+            if ele == ".":
                 my_col.append(None)
             else:
+                counter += 1
                 number_of_bridges = int(ele)
-                my_col.append(Island(r, c, number_of_bridges))
+                my_col.append(Island(r, c, number_of_bridges, chr(counter + 64)))
         islands.append(my_col)
     return islands
 
@@ -34,7 +40,10 @@ def _find_bridge(target_island, islands, puzzle_size):
     bridges = []
     directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
     for direction in directions:
-        searching_row, searching_col = target_island.x + direction[0], target_island.y + direction[1]
+        searching_row, searching_col = (
+            target_island.x + direction[0],
+            target_island.y + direction[1],
+        )
         while 0 <= searching_row < puzzle_size and 0 <= searching_col < puzzle_size:
             if islands[searching_row][searching_col] is not None:
                 bridges.append((target_island, islands[searching_row][searching_col]))
@@ -45,7 +54,9 @@ def _find_bridge(target_island, islands, puzzle_size):
     return bridges
 
 
-def read_puzzle_from_string(puzzle_str: str) -> Tuple[List[List[Island]], List[Tuple[Island, Island]]]:
+def read_puzzle_from_string(
+    puzzle_str: str,
+) -> Tuple[List[List[Island]], List[Tuple[Island, Island]]]:
     lines = puzzle_str.splitlines()
     puzzle_size = int(lines.pop(0))
     islands = _find_islands(lines)
@@ -57,15 +68,31 @@ def read_puzzle_from_string(puzzle_str: str) -> Tuple[List[List[Island]], List[T
     return islands, bridges
 
 
+def to_graph(
+    islands: List[List[Island]], bridges: List[Tuple[Island, Island]]
+) -> nx.MultiDiGraph:
+    g = nx.MultiDiGraph()
+    for rows in islands:
+        for island in rows:
+            if island is not None:
+                g.add_node(
+                    island, pos=(island.y, -island.x), label=island.number_of_bridges
+                )
+    g.add_edges_from(bridges)
+    return g
+
+
 # Draw the puzzle as graph
 def plot_puzzle(islands: List[List[Island]], bridges: List[Tuple[Island, Island]]):
     g = nx.MultiDiGraph()
     for rows in islands:
         for island in rows:
             if island is not None:
-                g.add_node(island, pos=(island.y, -island.x), label=island.number_of_bridges)
+                g.add_node(
+                    island, pos=(island.y, -island.x), label=island.number_of_bridges
+                )
     g.add_edges_from(bridges)
-    pos = nx.get_node_attributes(g, 'pos')
-    labels = nx.get_node_attributes(g, 'label')
+    pos = nx.get_node_attributes(g, "pos")
+    labels = nx.get_node_attributes(g, "label")
     nx.draw(g, pos=pos, with_labels=True, labels=labels)
     plt.show()
