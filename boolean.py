b35d8f2b319733077358b4745cf64fa0faa5aa9b
Lukas Mahr <Lukas.Mahr@campus.lmu.de>
2023-11-22 18:42:19 +0100

    Add functions to transform an cnf ast to a string that a sat solver likes.
    there is a recursice one that only works for small ast, because for
    big one we exeed the maximum recursion limit.
    therefor we also have an iterative one that uses 2 stacks to traverse
    the ast

--- boolean.py
+++ boolean.py
@@ -1,3 +1,6 @@
+from typing import Dict, Tuple
+
+
 class Node:
     pass
 
@@ -50,3 +53,85 @@ class Literal(Node):
 
     def __hash__(self):
         return hash(self.name)
+
+
+def to_solver_string_recursive(root):
+    # Maps literals to integers
+    literal_map = {}
+    next_literal_id = 1
+
+    def dfs(node):
+        nonlocal next_literal_id
+        if isinstance(node, AndNode):
+            return dfs(node.left) + " 0\n" + dfs(node.right)
+        elif isinstance(node, OrNode):
+            return dfs(node.left) + " " + dfs(node.right)
+        elif isinstance(node, NotNode):
+            return "-" + dfs(node.operand)
+        elif isinstance(node, Literal):
+            if node.name not in literal_map:
+                literal_map[node.name] = str(next_literal_id)
+                next_literal_id += 1
+            return literal_map[node.name]
+        else:
+            raise ValueError("Unknown node type")
+
+    return dfs(root) + " 0", {v: k for k, v in literal_map.items()}
+
+
+def to_solver_string_iterative(
+    root: Literal | AndNode | OrNode | NotNode,
+) -> Tuple[str, Dict[str, str]]:
+    """
+    Transforms a boolean expression in cnf to a string that can be used by a SAT solver.
+    We need to use an iterative approach because the recursive approach would exceed the recursion limit.
+    """
+    # Mapping to revers the process after the SAT solver has found a solution
+    literal_map = {}
+    next_literal_id = 1
+
+    # One stack for the Ast and one for the results
+    stack = [(root, False)]
+    result = []
+
+    while stack:
+        node, visited = stack.pop()
+
+        if visited:
+            # popping the node stack and build up the result stack
+            if isinstance(node, Literal):
+                if node.name not in literal_map:
+                    literal_map[node.name] = str(next_literal_id)
+                    next_literal_id += 1
+                result.append(literal_map[node.name])
+            elif isinstance(node, NotNode):
+                result.append(f"-{result.pop()}")
+            elif isinstance(node, AndNode):
+                right = result.pop()
+                left = result.pop()
+                result.append(f"{left} 0\n{right}")
+            elif isinstance(node, OrNode):
+                right = result.pop()
+                left = result.pop()
+                result.append(f"{left} {right}")
+            else:
+                # if for some reason we encounter an unknown node type, we raise an error
+                # but this should never happen
+                raise ValueError(f"Unknown node type, {node}")
+        else:
+            # Build up the stack of all nodes
+            if isinstance(node, Literal):
+                stack.append((node, True))
+            elif isinstance(node, NotNode):
+                stack.append((node, True))
+                stack.append((node.operand, False))
+            elif isinstance(node, AndNode) or isinstance(node, OrNode):
+                stack.append((node, True))
+                stack.append((node.right, False))
+                stack.append((node.left, False))
+            else:
+                # if for some reason we encounter an unknown node type, we raise an error
+                # but this should never happen
+                raise ValueError(f"Unknown node type, {node}")
+
+    return result[0] + " 0", {v: k for k, v in literal_map.items()}
